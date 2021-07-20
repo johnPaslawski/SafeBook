fe3c1057f7638de25069971e098e5b01043230b6
bart capote <gauclee@gmail.com>
2021-07-20 19:18:49 +0200

    Add in-memory database provider

--- SafeBook/SafeBook/Startup.cs
+++ SafeBook/SafeBook/Startup.cs
@@ -11,6 +11,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SafeBook.EfCoreInMemory;
 
 namespace SafeBook
 {
@@ -28,6 +30,7 @@ namespace SafeBook
         {
 
             services.AddControllers();
+            services.AddDbContext<InMemoryDbContext>(options => options.UseInMemoryDatabase("SafeBookInMemoryDb"));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "SafeBook", Version = "v1" });
---
+++ SafeBook/Safebook.EfCoreInMemory/InMemoryDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SafeBook.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace SafeBook.EfCoreInMemory
+{
+    public class InMemoryDbContext : DbContext
+    {
+        public InMemoryDbContext(DbContextOptions<InMemoryDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<News> News { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Offer> Offers { get; set; }
+    }
+}
