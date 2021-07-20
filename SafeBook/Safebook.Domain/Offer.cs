105fa0d19f2a8d95c72fb2b09e85ce20731212c3
bart capote <gauclee@gmail.com>
2021-07-20 19:06:41 +0200

    Add Domain class library with some models according to UML

---
+++ SafeBook/Safebook.Domain/Offer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafeBook.Domain
+{
+    public class Offer : BaseModel
+    {
+        public string Description { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ SafeBook/Safebook.Domain/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafeBook.Domain
+{
+    public class Project : BaseModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string ImageName { get; set; }
+    }
+}
