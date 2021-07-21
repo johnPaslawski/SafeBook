01f9904f73571c6abc858bc8d3abbac4bd1f9a98
bart capote <gauclee@gmail.com>
2021-07-21 20:29:09 +0200

    Add repository interfaces to domain library

---
+++ SafeBook/Safebook.Domain/Persistence/INewsRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Safebook.Domain.Persistence;
+
+namespace SafeBook.Domain.Persistence
+{
+    public interface INewsRepository : IRepository<News>
+    {
+        // Queries go here:
+    }
+}
---
+++ SafeBook/Safebook.Domain/Persistence/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using SafeBook.Domain;
+
+namespace Safebook.Domain.Persistence
+{
+    public interface IRepository<TEntity> where TEntity : BaseModel
+    {
+        TEntity Get(int id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+        void Add(TEntity entity);
+        void AddRange(IEnumerable<TEntity> entities);
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
+    }
+}
