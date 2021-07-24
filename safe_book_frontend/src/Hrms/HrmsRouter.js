769dbb2b17a11bca0e78af8ff228e60ba84f2126
kyrylpopyk <53974461+kyrylpopyk@users.noreply.github.com>
2021-07-24 12:04:13 +0200

    Creating folders and base files for HRMS and Shop modules

---
+++ safe_book_frontend/src/Hrms/HrmsRouter.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Switch, Route} from 'react-router-dom';
+
+const HrmsRouter = () => {
+    return(
+        <div>
+            <Switch>
+                <Route exact path="/hrms">
+                    
+                </Route>
+            </Switch>
+        </div>
+    );
+}
+
+
+export default HrmsRouter;
---
+++ safe_book_frontend/src/Shop/ShopRouter.js
@@ -0,0 +1,15 @@
+import { BrowserRouter as Switch, Route} from 'react-router-dom';
+
+const ShopRouter = () => {
+    return(
+        <div>
+            <Switch>
+                <Route exact path="/shop">
+                    
+                </Route>
+            </Switch>
+        </div>
+    );
+};
+
+export default ShopRouter;
