9e0b6d4104391888cf3f930d6aec30b67119833e
kyrylpopyk <53974461+kyrylpopyk@users.noreply.github.com>
2021-07-25 13:10:00 +0200

    Base configuration existing files v4 and delete unnecessary files

--- safe_book_frontend/src/index.js
+++ safe_book_frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import AppRouter from './App';
+import AppRouter from './App/App';
 
 ReactDOM.render(
   <React.StrictMode>
