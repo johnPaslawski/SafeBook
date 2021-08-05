565b69f78e0c1f5d7de4920b301482a09ca3662a
kyrylpopyk <53974461+kyrylpopyk@users.noreply.github.com>
2021-08-06 00:07:25 +0200

    Added cors service to allow CORS police

--- SafeBook/SafeBook/Startup.cs
+++ SafeBook/SafeBook/Startup.cs
@@ -24,7 +24,16 @@ namespace SafeBook
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
+
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder.WithOrigins("http://localhost:3000");
+                    });
+            });
+
             services.AddControllers()
                 .AddNewtonsoftJson(x =>
             {
@@ -50,6 +59,8 @@ namespace SafeBook
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -67,8 +78,6 @@ namespace SafeBook
             {
                 endpoints.MapControllers();
             });
-
-            
 
         }
         
