49541b2e7d1eff3de1453d89a4dddd67835d4c1b
bart capote <gauclee@gmail.com>
2021-09-20 21:56:06 +0200

    Add missing IdentityUser reference to Identity DbContext

--- SafeBook/Safebook.EfCore/EFData/SafeBookIdentityDbContext.cs
+++ SafeBook/Safebook.EfCore/EFData/SafeBookIdentityDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace SafeBook.EfCore.EFData
 {
-    public class SafeBookIdentityDbContext : IdentityDbContext
+    public class SafeBookIdentityDbContext : IdentityDbContext<IdentityUser>
     {
         public SafeBookIdentityDbContext(DbContextOptions<SafeBookIdentityDbContext> options)
             : base(options)
