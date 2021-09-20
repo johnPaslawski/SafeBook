580bc001c1fdb039b8915fa2f2743fac9bd7cebf
bart capote <gauclee@gmail.com>
2021-09-20 20:04:20 +0200

    Add Identity DbContext

---
+++ SafeBook/Safebook.EfCore/EFData/SafeBookIdentityDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SafeBook.EfCore.EFData
+{
+    public class SafeBookIdentityDbContext : IdentityDbContext
+    {
+        public SafeBookIdentityDbContext(DbContextOptions<SafeBookIdentityDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
