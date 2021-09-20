fb6a9d934fbe5115f847c7f34f1596f723f20835
bart capote <gauclee@gmail.com>
2021-09-20 21:24:43 +0200

    Add controllers and viewmodels for IdentityServer

---
+++ SafeBook/SafeBook.IdentityServer/Controllers/AuthController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace SafeBook.IdentityServer.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IIdentityServerInteractionService _interactionService;
+
+        public AuthController(SignInManager<IdentityUser> signInManager,
+            UserManager<IdentityUser> userManager,
+            IIdentityServerInteractionService interactionService)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _interactionService = interactionService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login(string returnUrl)
+        {
+            return View(new LoginViewModel() { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel viewModel)
+        {
+            // TODO Validate model !!!
+            // ...
+
+            var result = await _signInManager.PasswordSignInAsync(viewModel.Username, viewModel.Password, false, false);
+
+            if (result.Succeeded)
+            {
+                return Redirect(viewModel.ReturnUrl);
+            }
+
+            if (result.IsLockedOut)
+            {
+                // TODO send recovery email or similar
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Register(string returnUrl)
+        {
+            return View(new RegisterViewModel(){ ReturnUrl = returnUrl});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel viewModel)
+        {
+            // TODO Validate model !!!
+            // ...
+
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            var user = new IdentityUser(viewModel.Username);
+            var result = await _userManager.CreateAsync(user, viewModel.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return Redirect(viewModel.ReturnUrl);
+            }
+
+            return View();
+        }
+    }
+}
--- SafeBook/SafeBook.IdentityServer/Startup.cs
+++ SafeBook/SafeBook.IdentityServer/Startup.cs
@@ -1,12 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -45,6 +40,8 @@ namespace SafeBook.IdentityServer
             });
 
             services.AddIdentityServer();
+
+            services.AddControllersWithViews();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
